ef059cc6b10b1802791ccad314a51176d6367d31
sanja <sanja@Sanju>
2023-06-21 13:09:34 +0530

    [21st June 23]:- POM_DDF_TestNG

--- src/TestNG/example12_MultiBrowserTesting.java
+++ src/TestNG/example12_MultiBrowserTesting.java
@@ -12,27 +12,30 @@ public class example12_MultiBrowserTesting
 	
 	@Parameters("browserName")
 	@Test
-	public void TC1(String browserName) throws InterruptedException 
+	public void TC1(String browserName) throws InterruptedException
 	{
 		WebDriver driver=null;
 		
 		if(browserName.equals("chrome"))
 		{
-			  driver=new ChromeDriver();
+			driver=new ChromeDriver();
 		}
-		else if (browserName.equals("firefox"))
+		else if (browserName.equals("firefox")) 
 		{
-			 driver=new FirefoxDriver();
+			driver=new FirefoxDriver();
 		}
-		else if (browserName.equals("edge")) 
+		else if (browserName.equals("edge"))
 		{
-			 driver=new EdgeDriver();
+			driver=new EdgeDriver();
 		}
 		
 		driver.get("https://www.facebook.com/");
-		//driver.manage().window().maximize();
-		Thread.sleep(4000);
-		driver.quit();	
+		driver.manage().window().maximize();
+		
+		Thread.sleep(3000);
+
+		driver.close();
+		
 	}
 
 }
